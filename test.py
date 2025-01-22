c49c98ab340c824acf10afe60a15f6a979516748
jhuniorbryan <corvinobrayan@gmail.com>
2025-01-21 23:18:54 -0500

    commit test.py desde JhuniorBrayan

---
+++ test.py
@@ -0,0 +1,29 @@
+element = 83
+element_2 = 5
+
+modulo = element % element_2
+print(modulo)
+
+# Ejemplo 3: Obtener el último dígito de un número
+
+numero = 12349
+ultimo_digito = numero % 10
+print(f"El último dígito de {numero} es {ultimo_digito}.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
